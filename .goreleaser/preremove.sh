39600a13eda298ada835db6af098213e09818f2d
Olivier Poitrey <rs@nextdns.io>
2019-12-02 21:22:29 -0800

    Do not unstall nextdns on upgrade on Debian based distribs

--- .goreleaser/preremove.sh
+++ .goreleaser/preremove.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# For deb, ignore upgrade actions
+case "$1" in
+upgrade|failed-upgrade|deconfigure) exit 0;;
+esac
+
 if nextdns status > /dev/null 2>&1; then
     nextdns uninstall
 fi
