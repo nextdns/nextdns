66e4776094362eb679e5c7747a975a6574fe6a61
Olivier Poitrey <rs@nextdns.io>
2019-12-05 21:59:57 -0800

    Trim return on macOS hostname

--- host/name_darwin.go
+++ host/name_darwin.go
@@ -3,6 +3,7 @@
 package host
 
 import (
+	"bytes"
 	"os"
 	"os/exec"
 	"strings"
@@ -11,7 +12,7 @@ import (
 func Name() (string, error) {
 	b, err := exec.Command("networksetup", "-getcomputername").Output()
 	if err == nil {
-		return string(b), nil
+		return string(bytes.TrimSpace(b)), nil
 	}
 	h, err := os.Hostname()
 	return strings.TrimSuffix(h, ".local"), err
