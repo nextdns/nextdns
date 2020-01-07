5c659cd65aad914849faef98de903c9fd96935d3
Olivier Poitrey <rs@nextdns.io>
2020-01-06 22:45:16 -0800

    Remove failing upx (for now)

--- .goreleaser/postbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-for arch in arm64 arm_5 arm_6 arm_7 mips mipsle; do
-    bin="dist/nextdns_linux_$arch/nextdns"
-    echo "      • compressing                  $bin"
-    upx -q --brute $bin
-done
