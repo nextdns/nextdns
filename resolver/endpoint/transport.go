98467cf61ace4d6a1776a16cb75f5768e4be0d51
Olivier Poitrey <rs@nextdns.io>
2020-04-04 23:58:03 -0700

    Fix stdlib happy eyeball not disabled correctly
    Fixes #125

--- resolver/endpoint/transport.go
+++ resolver/endpoint/transport.go
@@ -27,7 +27,7 @@ func newTransport(e *DOHEndpoint) transport {
 		addr = e.Hostname
 	}
 	d := &parallelDialer{}
-	d.FallbackDelay = 0 // disable happy eyeball, we do our own
+	d.FallbackDelay = -1 // disable happy eyeball, we do our own
 	t := &http.Transport{
 		TLSClientConfig: &tls.Config{
 			ServerName: e.Hostname,
