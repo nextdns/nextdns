fcf3fdc65b1d933b0f7f45e62484d2c9716273e1
Olivier Poitrey <rs@nextdns.io>
2020-04-16 13:33:21 -0700

    service/internal.RunOutput: fix blocking on certain platforms
    Fixes #156

--- host/service/internal/run_unix.go
+++ host/service/internal/run_unix.go
@@ -1,10 +1,11 @@
 package internal
 
 import (
-	"bytes"
+	"bufio"
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"os/exec"
 	"strings"
 	"syscall"
@@ -17,15 +18,37 @@ func Run(command string, arguments ...string) error {
 }
 
 func RunOutput(command string, args ...string) (string, error) {
+	var stdout string
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	cmd := exec.CommandContext(ctx, command, args...)
-	stdout, err := cmd.Output()
+	// Obtain a pipe connected to our cmd stdout so we can read from it later.
+	// cmd.Output() would block here because the underlying pipes are not closed
+	// and it will try to read from them indefinitely
+	stdoutPipe, err := cmd.StdoutPipe()
 	if err != nil {
 		cancel()
 		return "", fmt.Errorf("%s %s: %w", command, strings.Join(args, " "), err)
 	}
-	return string(bytes.TrimSpace(stdout)), nil
+	if err = cmd.Start(); err != nil {
+		cancel()
+		return "", fmt.Errorf("%s %s: %w", command, strings.Join(args, " "), err)
+	}
+	go func(stdout string, stdoutPipe io.ReadCloser) {
+		buf := bufio.NewReader(stdoutPipe)
+		for {
+			line, _ := buf.ReadString('\n')
+			if len(line) > 0 {
+				stdout = stdout + line + "\n"
+			}
+		}
+	}(stdout, stdoutPipe)
+	err = cmd.Wait()
+	if err != nil {
+		cancel()
+		return "", fmt.Errorf("%s %s: %w", command, strings.Join(args, " "), err)
+	}
+	return stdout, nil
 }
 
 func ExitCode(err error) int {
