39fc2c9f37e44f49f6ed8c91d28fa24c8cfd2423
Olivier Poitrey <rs@nextdns.io>
2020-04-23 15:20:23 -0700

    Support both sed -E and sed -r variants

--- install.sh
+++ install.sh
@@ -815,8 +815,16 @@ get_release() {
         fi
         $curl "https://api.github.com/repos/nextdns/nextdns/releases/latest" |
             grep '"tag_name":' |
-            sed -E 's/.*"([^"]+)".*/\1/' |
+            esed 's/.*"([^"]+)".*/\1/' |
             sed -e 's/^v//'
+    fi
+}
+
+esed() {
+    if (echo | sed -E '' >/dev/null 2>&1); then
+        sed -E "$@"
+    else
+        sed -r "$@"
     fi
 }
 
