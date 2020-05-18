0e79af0826d3e89eb0a760ecbe4f536d325f774b
Olivier Poitrey <rs@nextdns.io>
2020-05-18 00:42:48 -0700

    Add missing package

---
+++ cache/cache.go
@@ -0,0 +1,43 @@
+package cache
+
+import (
+	"sync/atomic"
+
+	lru "github.com/hashicorp/golang-lru"
+)
+
+type Cache struct {
+	*lru.ARCCache
+
+	hit, miss uint64
+}
+
+type Stats struct {
+	Hit  uint64 `json:"hit"`
+	Miss uint64 `json:"miss"`
+}
+
+func New(size int) (*Cache, error) {
+	arc, err := lru.NewARC(size)
+	if err != nil {
+		return nil, err
+	}
+	return &Cache{ARCCache: arc}, nil
+}
+
+func (c *Cache) Get(key interface{}) (value interface{}, ok bool) {
+	value, ok = c.ARCCache.Get(key)
+	if ok {
+		atomic.AddUint64(&c.hit, 1)
+	} else {
+		atomic.AddUint64(&c.miss, 1)
+	}
+	return value, ok
+}
+
+func (c *Cache) Stats() Stats {
+	return Stats{
+		Hit:  atomic.LoadUint64(&c.hit),
+		Miss: atomic.LoadUint64(&c.miss),
+	}
+}
