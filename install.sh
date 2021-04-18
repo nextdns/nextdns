55e0a348a995cc78072ade69bdd11ba984d89fff
Vincent Bernat <vincent@bernat.ch>
2021-04-18 09:52:18 +0200

    Support OpenSUSE Leap in installer
    OpenSUSE Leap is basically the opensource stable version of SuSE. In
    the future, it should even be on par with SLE. Handle it like any
    other SuSE distribution (with zypper).

--- install.sh
+++ install.sh
@@ -456,7 +456,7 @@ install_type() {
     centos|fedora|rhel)
         echo "rpm"
         ;;
-    opensuse-tumbleweed|opensuse)
+    opensuse-tumbleweed|opensuse-leap|opensuse)
         echo "zypper"
         ;;
     debian|ubuntu|elementary|raspbian|linuxmint|pop|neon|sparky|vyos)
@@ -821,7 +821,7 @@ detect_os() {
                 fi
                 echo "$dist"; return 0
                 ;;
-            debian|ubuntu|elementary|raspbian|centos|fedora|rhel|arch|manjaro|openwrt|clear-linux-os|linuxmint|opensuse-tumbleweed|opensuse|solus|pop|neon|overthebox|sparky|vyos)
+            debian|ubuntu|elementary|raspbian|centos|fedora|rhel|arch|manjaro|openwrt|clear-linux-os|linuxmint|opensuse-tumbleweed|opensuse-leap|opensuse|solus|pop|neon|overthebox|sparky|vyos)
                 echo "$dist"; return 0
                 ;;
             esac
@@ -905,7 +905,7 @@ silent_exec() {
 
 bin_location() {
     case $OS in
-    centos|fedora|rhel|debian|ubuntu|elementary|raspbian|arch|manjaro|clear-linux-os|linuxmint|opensuse-tumbleweed|opensuse|solus|pop|neon|sparky|vyos)
+    centos|fedora|rhel|debian|ubuntu|elementary|raspbian|arch|manjaro|clear-linux-os|linuxmint|opensuse-tumbleweed|opensuse-leap|opensuse|solus|pop|neon|sparky|vyos)
         echo "/usr/bin/nextdns"
         ;;
     openwrt|overthebox)
