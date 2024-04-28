b6cc73447300ba68f20d5031ab79ad358e42b330
Olivier Poitrey <rs@nextdns.io>
2024-04-28 03:51:45 +0200

    Fix logging on macOS
    Fixes #666

--- host/log_darwin.go
+++ host/log_darwin.go
@@ -1,18 +1,71 @@
 package host
 
 import (
-	"errors"
+	"fmt"
+	"os"
 	"os/exec"
 )
 
+//#include <syslog.h>
+//void doLog(int facility, const char *msg) {
+//	syslog(facility, "%s", msg);
+//}
+import "C"
+
+const kLOG_DAEMON = 0x18
+const kLOG_ERR = 0x3
+const kLOG_WARNING = 0x4
+const kLOG_INFO = 0x6
+const kLOG_DEBUG = 0x7
+
+type macosLogger struct{}
+
+func (l macosLogger) log(facility int, msg string) {
+	C.doLog(C.int(facility|kLOG_DAEMON), C.CString(msg))
+}
+
+func (l macosLogger) Debug(v ...interface{}) {
+	l.log(kLOG_DEBUG, fmt.Sprint(v...))
+}
+
+func (l macosLogger) Debugf(format string, a ...interface{}) {
+	l.log(kLOG_DEBUG, fmt.Sprintf(format, a...))
+}
+
+func (l macosLogger) Info(v ...interface{}) {
+	l.log(kLOG_INFO, fmt.Sprint(v...))
+}
+
+func (l macosLogger) Infof(format string, a ...interface{}) {
+	l.log(kLOG_INFO, fmt.Sprintf(format, a...))
+}
+
+func (l macosLogger) Warning(v ...interface{}) {
+	l.log(kLOG_WARNING, fmt.Sprint(v...))
+}
+
+func (l macosLogger) Warningf(format string, a ...interface{}) {
+	l.log(kLOG_WARNING, fmt.Sprintf(format, a...))
+}
+
+func (l macosLogger) Error(v ...interface{}) {
+	l.log(kLOG_ERR, fmt.Sprint(v...))
+}
+
+func (l macosLogger) Errorf(format string, a ...interface{}) {
+	l.log(kLOG_ERR, fmt.Sprintf(format, a...))
+}
+
 func newServiceLogger(name string) (Logger, error) {
-	return newSyslogLogger(name)
+	return macosLogger{}, nil
 }
 
 func ReadLog(process string) ([]byte, error) {
-	return exec.Command("grep", process, "/var/log/system.log").Output()
+	return exec.Command("log", "show", "--info", "--debug", "--process", process, "--no-pager", "--style", "syslog").Output()
 }
 
-func FollowLog(name string) error {
-	return errors.New("-f/--follow not implemented")
+func FollowLog(process string) error {
+	cmd := exec.Command("log", "stream", "--level", "debug", "--process", process, "--style", "syslog")
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
 }
