df7d2e668202336252ad615233f8a43fdbf7c105
Olivier Poitrey <rs@nextdns.io>
2024-04-28 02:20:17 +0200

    Remove netlink dependency for ndp on linux

--- go.mod
+++ go.mod
@@ -6,9 +6,6 @@ require (
 	github.com/cespare/xxhash v1.1.0
 	github.com/denisbrodbeck/machineid v1.0.1
 	github.com/hashicorp/golang-lru v1.0.2
-	github.com/vishvananda/netlink v1.1.0
 	golang.org/x/net v0.24.0
 	golang.org/x/sys v0.19.0
 )
-
-require github.com/vishvananda/netns v0.0.4 // indirect
--- ndp/ndp_linux.go
+++ ndp/ndp_linux.go
@@ -1,24 +1,41 @@
+//go:build linux
 // +build linux
 
 package ndp
 
 import (
-	"github.com/vishvananda/netlink"
+	"bufio"
+	"net"
+	"os/exec"
+	"strings"
 )
 
-func Get() (Table, error) {
-	neights, err := netlink.NeighList(0, netlink.FAMILY_V6)
+func Get() (t Table, err error) {
+	cmd := exec.Command("ip", "-6", "neigh")
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return nil, err
+		return t, err
 	}
 
-	var t Table
-	for _, n := range neights {
-		t = append(t, Entry{
-			IP:  n.IP,
-			MAC: n.HardwareAddr,
-		})
+	if err := cmd.Start(); err != nil {
+		return t, err
 	}
 
-	return t, nil
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+		if len(parts) >= 5 {
+			ip := net.ParseIP(parts[0])
+			mac, _ := net.ParseMAC(parts[4])
+			if ip != nil && mac != nil {
+				t = append(t, Entry{
+					IP:  ip,
+					MAC: mac,
+				})
+			}
+		}
+	}
+
+	return t, cmd.Wait()
 }
